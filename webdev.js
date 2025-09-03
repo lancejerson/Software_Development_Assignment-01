0fdc9ff6c9bcea75bced23650ebdc578717781fb
lance jerson <lancejersondemonteverde@gmail.com>
2025-09-03 10:28:19 +0800

    updated function

--- webdev.js
+++ webdev.js
@@ -43,6 +43,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    //modified code
+
     editButton.addEventListener('click', function() {
         enableEditMode();
     });
